f1d5154fde161bdf44fe2714a4ca749f417865d8
VincentCoatmellec <vincent.coatmellec@gmail.com>
2026-08-25 14:32:17 +0200

    Move SECRET_KEY to environment variable

--- src/CurrencyDashboard/urls.py
+++ src/CurrencyDashboard/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from currency.views import dashboard
 
 urlpatterns = [
+    path('', dashboard),
     path('admin/', admin.site.urls),
 ]
