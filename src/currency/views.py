47007f7994874f36744b0edbd94c4e8e36e3a4e7
VincentCoatmellec <vincent.coatmellec@gmail.com>
2026-08-26 13:50:27 +0200

    first graph with chartJS and bootstrap

--- src/currency/views.py
+++ src/currency/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+import api
+
 
 def dashboard(request):
-    return render(request, "currency/index.html", context={"test": range(10)})
+    days, rates = api.get_rates(currencies=["USD"], days=30)
+
+    return render(request, "currency/index.html", context={"data": rates["USD"],
+                                                           "days_labels": days})
